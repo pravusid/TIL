f6ae75743d6420bf44faf87e1efa063ac0f7bb8f
Sangdon Park <pravusid@gmail.com>
2018-08-03 17:15:40 +0900

    add: golang stack

---
+++ Go/training/calculator/Calculator.go
@@ -0,0 +1,63 @@
+package calculator
+
+import (
+	"strconv"
+	"strings"
+)
+
+// Eval 주어진 표현식의 실행 값을 반환함
+func Eval(expr string) int {
+	var ops []string
+	var nums []int
+	// 함수 리터럴 사용 (익명함수)
+	pop := func() int {
+		last := nums[len(nums)-1]
+		nums = nums[:len(nums)-1]
+		return last
+	}
+	reduce := func(higher string) {
+		for len(ops) > 0 {
+			op := ops[len(ops)-1]
+			if strings.Index(higher, op) < 0 {
+				// 목록에 없는 연산자
+				return
+			}
+			ops = ops[:len(ops)-1]
+			if op == "(" {
+				// 괄호제거
+				return
+			}
+			b, a := pop(), pop()
+			switch op {
+			case "+":
+				nums = append(nums, a+b)
+			case "-":
+				nums = append(nums, a-b)
+			case "*":
+				nums = append(nums, a*b)
+			case "/":
+				nums = append(nums, a/b)
+			}
+		}
+	}
+	for _, token := range strings.Split(expr, "") {
+		switch token {
+		case "(":
+			ops = append(ops, token)
+		case "+", "-":
+			// 덧셈과 뺄셈 이상의 우선순위
+			reduce("+-*/")
+			ops = append(ops, token)
+		case "*", "/":
+			reduce("*/")
+			ops = append(ops, token)
+		case ")":
+			reduce("+-*/(")
+		default:
+			num, _ := strconv.Atoi(token)
+			nums = append(nums, num)
+		}
+	}
+	reduce("+-*/")
+	return nums[0]
+}
---
+++ Go/training/calculator/Calculator_test.go
@@ -0,0 +1,11 @@
+package calculator
+
+import "fmt"
+
+func ExampleEval() {
+	fmt.Println(Eval("5"))
+	fmt.Println(Eval("1+2"))
+	// Output:
+	// 5
+	// 3
+}
